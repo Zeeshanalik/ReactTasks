9a86712022abad784d6fe8b74acbfa2505840b91
Zeeshanalik <zeeshan.ali@datumbrain.com>
2021-03-22 18:50:42 +0500

    Made Form Editable

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,56 @@
 import React, {useState} from "react";
-function Form({addDetails,person, setPerson}){
+function Form({addDetails,person, setPerson,edit,setEdit, detailsList, setDetailsList, arrIndex, setArrIndex,id,setId}){
     const handleClick = (e) => {
         e.preventDefault();
-        addDetails(person.name, person.fatherName, person.age, person.address);
-        setPerson({name: "", fatherName: "", age: 0, address: ""})
+        
+        if(person.name==="" || person.fatherName === "" || person.address ==="" || person.age===0) {
+            alert("Error! Please enter your details again correctly!")     
+           }else if (person.id === detailsList.id){
+               alert("Please Enter a unique id")
+           }
+           else addDetails(person.id, person.name, person.fatherName, person.age, person.address);
+
+           if (edit===true){
+            let newArr = [...detailsList];
+            console.log("id is:",id);
+            const updatedArray = newArr.map(value => {
+                if (value.id === id){
+                    return {
+                        id: id,
+                        name: person.name,
+                        fatherName: person.fatherName,
+                        age: person.age,
+                        address: person.address
+                        
+                    }
+                } else return value;
+            })
+            console.log('Updated Array', updatedArray)
+            setDetailsList(updatedArray);
+            setEdit(false)
+    }
+        setPerson({id:0 , name: "", fatherName: "", age: 0, address: ""})
 
     }
-
     
     return(
         <div>
-            <form>
+            <form className="login-form">
+            <label>Please Enter Your Id </label>
+            <input type="number" 
+            placeholder="Id" 
+            value={person.id} 
+            onChange={(e) => setPerson({...person, id:e.target.value})}
+            />
+            <hr/>
 
             <label>Please Enter Your Name: </label>
             <input type="text" 
             placeholder="First Name" 
             value={person.name} 
-            onChange={(e) => setPerson({...person, name:e.target.value})}/>
+            onChange={(e) => setPerson({...person, name:e.target.value})}
+            
+            />
             <hr/>
 
             <label>Please Enter Your Father Name: </label>
@@ -39,7 +73,7 @@ function Form({addDetails,person, setPerson}){
             value={person.address} 
             onChange={(e) => setPerson({...person, address:e.target.value})}/>
             <br/>
-            <button onClick={handleClick}> Submit</button>
+            <button className="btn" onClick={handleClick}> Submit</button>
 
               </form>
         </div>
@@ -47,5 +81,5 @@ function Form({addDetails,person, setPerson}){
     )
 
 
-}
-export default Form
+
+}export default Form
--- src/components/FormTable.js
+++ src/components/FormTable.js
@@ -1,12 +1,14 @@
 import React from "react";
-function FormTable({detailsList,setPerson, person}) {
-    const handleEdit=(key)=>{
-        detailsList.filter((v,i) => {if (key===i) {
-            detailsList[v] = setPerson({name: ""})
-            console.log(i)
-            
-            
-        }})
+function FormTable({detailsList,setPerson, person,addDetails,edit,setEdit,setDetailsList,arrIndex,setArrIndex, id, setId}) {
+    
+    const handleEdit=(detId)=>{
+    
+     setEdit(true)
+    const tempObj = detailsList.filter((value) =>  (detId === value.id))[0]
+    setId(detId)
+    setPerson(tempObj) 
+
+    
     }
  return (
      <>
@@ -14,25 +16,27 @@ function FormTable({detailsList,setPerson, person}) {
         <table>
             <thead>
                 <tr>
-                    <th>Full Name</th>
-                    <th>Father's Name</th>
-                    <th>Age</th>
-                    <th>Address</th>
+                    <th>ID |</th>
+                    <th>Full Name |</th>
+                    <th> Father's Name |</th>
+                    <th> Age |</th>
+                    <th> Address </th>
                 </tr>
             </thead>
             <tbody>
-                {detailsList.map((detail,key) => (
+                {detailsList.map((detail) => (
                     <tr>
+                        <td>{detail.id}</td>
                         <td>{detail.name}</td>
                         <td>{detail.fatherName}</td>
                         <td>{detail.age}</td>
                         <td>{detail.address}</td>
-                        <td><button onClick={()=>handleEdit(key)}>Edit</button></td>
+                        <td><button onClick={()=>handleEdit(detail.id)}>Edit</button></td>
                     </tr>
                 ))}
             </tbody>
         </table>
     </div></>
- )
+ )  
 }
 export default FormTable
